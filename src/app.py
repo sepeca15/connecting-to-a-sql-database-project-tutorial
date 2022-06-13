094569014cd93be59bfd9c9120b8e5cb2e0a1d7c
sepeca15 <sepeca15@gmail.com>
2022-06-13 23:54:18 +0000

    Todo listo para usar la BD

--- src/app.py
+++ src/app.py
@@ -16,15 +16,15 @@ def connect():
     return engine
 connect()
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-#engine.execute("""
-#CREATE TABLE publishers(
-#publisher_id INT NOT NULL,
-#name VARCHAR(255) NOT NULL,
-#PRIMARY KEY(publisher_id)
-#);
-#""")
+engine.execute("""
+CREATE TABLE publishers(
+publisher_id INT NOT NULL,
+name VARCHAR(255) NOT NULL,
+PRIMARY KEY(publisher_id)
+);
+""")
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-#engine.execute("INSERT INTO publishers(publisher_id,name) values (1,'O Reilly Media');")
+engine.execute("INSERT INTO publishers(publisher_id,name) values (1,'O Reilly Media');")
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
 result_dataFrame = pd.read_sql("Select * from publishers;", engine)
 print(result_dataFrame)
